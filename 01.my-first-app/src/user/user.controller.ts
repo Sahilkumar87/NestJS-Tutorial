0717d3b5e0bb5c16bdaae310018b874b9b104011
sahil kumar <sahilkumar06804@gmail.com>
2026-09-15 18:05:39 +0530

    controller and api routes

---
+++ 01.my-first-app/src/user/user.controller.ts
@@ -0,0 +1,10 @@
+import { Controller, Get } from '@nestjs/common';
+import { get } from 'http';
+
+@Controller('user') // Decorator
+export class UserController {
+    @Get()
+    getUser(){
+        return 'User data feteched successfully!';
+    }
+}
